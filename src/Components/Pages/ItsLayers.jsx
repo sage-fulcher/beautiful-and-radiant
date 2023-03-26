fba72763338b15da46046b55afc284dbdf967722
Sage Fulcher <42389235+sage-fulcher@users.noreply.github.com>
2023-03-25 20:37:51 -0400

    xp background

--- src/Components/Pages/ItsLayers.jsx
+++ src/Components/Pages/ItsLayers.jsx
@@ -1,28 +1,26 @@
-import { Box, Typography, withStyles } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 import React from 'react'
 import { DeadSimple } from '../Organisms/deadSimple'
-
-const WhiteTextTypography = withStyles({
-  root: {
-    color: '#FFFFFF',
-  },
-})(Typography)
 
 export const ItsLayers = () => {
   return (
     <Box
-      bgcolor={'#008080'}
+      // bgcolor={'#008080'}
       height={'96vh'}
       width={'96vw'}
       paddingY={'2vh'}
       paddingX={'2vw'}
       display={'flex'}
       flexDirection={'column'}
+      style={{
+        backgroundImage:
+          'url(https://www.newegg.com/insider/wp-content/uploads/windows_xp_bliss-wide.jpg)',
+      }}
     >
       <link rel="stylesheet" href="https://unpkg.com/xp.css"></link>
-      <WhiteTextTypography variant="h4">
+      <Typography variant="h4">
         The past, present and future are only illusions, even if stubborn ones
-      </WhiteTextTypography>
+      </Typography>
       <div
         className="window"
         style={{
