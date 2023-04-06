ef9189e14d818105ac84e69b9fa63686ce38fc49
Sage Fulcher <42389235+sage-fulcher@users.noreply.github.com>
2023-04-06 09:57:17 -0400

    normal distribution opacties

--- src/Components/Organisms/deadSimple.jsx
+++ src/Components/Organisms/deadSimple.jsx
@@ -7,6 +7,30 @@ const videoConstraints = {
   height: 480,
 }
 
+function getNormalSeries(num) {
+  const mean = 5
+  const stdDev = 2
+  let series = []
+  for (let i = 1; i <= 10; i++) {
+    if (i === num) {
+      series.push(0.2)
+    } else if (i === 1 || i === 10) {
+      series.push(0.01)
+    } else {
+      const probability = normalDistribution(mean, stdDev, i)
+      const value = probability * (1 - 0.2 - 0.01 * 2)
+      series.push(value)
+    }
+  }
+  return series
+}
+
+function normalDistribution(mean, stdDev, x) {
+  const coefficient = 1 / (stdDev * Math.sqrt(2 * Math.PI))
+  const exponent = -0.5 * Math.pow((x - mean) / stdDev, 2)
+  return coefficient * Math.exp(exponent)
+}
+
 export const DeadSimple = () => {
   const webcamRef = useRef(null)
   let srcs = useRef([])
@@ -32,6 +56,7 @@ export const DeadSimple = () => {
       srcs.current = appendedSrcs.slice(0, 10)
     }
   }, [newSrc])
+  const opacities = getNormalSeries(5)
 
   return (
     <Box
@@ -61,14 +86,14 @@ export const DeadSimple = () => {
           videoConstraints={videoConstraints}
         />
       </Box>
-      {srcs.current.map((src) => (
+      {srcs.current.map((src, idx) => (
         <Box
           width={'100%'}
           height={'100%'}
           paddingBottom={'50px'}
           zIndex={1}
           style={{
-            opacity: 0.1,
+            opacity: opacities[idx],
             position: 'absolute',
           }}
           display={'flex'}
